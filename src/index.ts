1af7879dc42d98247b7f689c10914025cd173e91
Jan Wyszynski <wyszynski@google.com>
2023-05-19 17:55:16 -0400

    Revert "initial commit"
    This reverts commit 2a0ef1c16ba34e6a233092e7e2ce9242ced7a0b6.

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,20 @@
-import { initializeApp, cert } from 'firebase-admin/app';
-import { getStorage } from 'firebase-admin/storage';
-import { DownloadResponse } from '@google-cloud/storage';
+import { initializeApp } from 'firebase/app';
+import { ref, getStorage, connectStorageEmulator, getDownloadURL } from 'firebase/storage';
 
 const config = {
   projectId: "demo-signed-urls",
-  credential: cert(require('../key.json')),
   storageBucket: "demo-signed-urls.appspot.com",
 };
 
-process.env["FIREBASE_STORAGE_EMULATOR_HOST"] = "127.0.0.1:9199"
-
 const app = initializeApp(config);
 const storage = getStorage(app);
+connectStorageEmulator(storage, "127.0.0.1", 9199);
 
 (async function() {
   try {
-    const bucket = storage.bucket()
-    const f: DownloadResponse = await bucket.file("c").download()
-    console.log(f[0].toString("utf-8"))
-    // The emulator doesn't support this.
-    // await bucket.getSignedUrl({ expires: 604800 } as GetBucketSignedUrlConfig)
+    const url = await getDownloadURL(ref(storage, "c"));
+    console.log(`${url}`)
   } catch (e) {
-    console.log(`ERROR: ${e}`);
+    console.log(`error getting download url: ${e}`);
   }
 })()
