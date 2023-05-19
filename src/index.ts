2a0ef1c16ba34e6a233092e7e2ce9242ced7a0b6
Jan Wyszynski <wyszynski@google.com>
2023-05-19 17:52:38 -0400

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
-import { initializeApp } from 'firebase/app';
-import { ref, getStorage, connectStorageEmulator, getDownloadURL } from 'firebase/storage';
+import { initializeApp, cert } from 'firebase-admin/app';
+import { getStorage } from 'firebase-admin/storage';
+import { DownloadResponse } from '@google-cloud/storage';
 
 const config = {
   projectId: "demo-signed-urls",
+  credential: cert(require('../key.json')),
   storageBucket: "demo-signed-urls.appspot.com",
 };
 
+process.env["FIREBASE_STORAGE_EMULATOR_HOST"] = "127.0.0.1:9199"
+
 const app = initializeApp(config);
 const storage = getStorage(app);
-connectStorageEmulator(storage, "127.0.0.1", 9199);
 
 (async function() {
   try {
-    const url = await getDownloadURL(ref(storage, "c"));
-    console.log(`${url}`)
+    const bucket = storage.bucket()
+    const f: DownloadResponse = await bucket.file("c").download()
+    console.log(f[0].toString("utf-8"))
+    // The emulator doesn't support this.
+    // await bucket.getSignedUrl({ expires: 604800 } as GetBucketSignedUrlConfig)
   } catch (e) {
-    console.log(`error getting download url: ${e}`);
+    console.log(`ERROR: ${e}`);
   }
 })()
